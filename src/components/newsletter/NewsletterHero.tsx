c14ba019dc7157c3f6f6d8da4289182553485234
Lawrence <74542269+LawrenceBaatjies@users.noreply.github.com>
2025-05-01 15:48:47 +0200

    Update NewsletterHero.tsx

--- src/components/newsletter/NewsletterHero.tsx
+++ src/components/newsletter/NewsletterHero.tsx
@@ -5,9 +5,9 @@ const NewsletterHero = () => {
   return (
     <div className="relative bg-church-navy text-white">
       <div 
-        className="absolute inset-0 bg-cover bg-center"
+        className="absolute inset-0 bg-cover bg-center py-30"
         style={{ 
-          backgroundImage: "url('https://images.unsplash.com/photo-1492321936769-b49830bc1d1e?q=80&w=2671&auto=format&fit=crop')", 
+          backgroundImage: "url('https://images-platform.99static.com//9H0jVKkEVk5d0szfDJ-9_6W5u5s=/109x122:1098x1113/fit-in/500x500/99designs-contests-attachments/107/107682/attachment_107682413')", 
           opacity: "0.4" 
         }}
       />
