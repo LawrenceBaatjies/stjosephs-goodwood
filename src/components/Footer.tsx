d82eb4c658f7351633ba7dda7a43759f5a4b039a
Lawrence <74542269+LawrenceBaatjies@users.noreply.github.com>
2025-05-18 15:00:09 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,15 +16,15 @@ const Footer = () => {
             <ul className="space-y-3">
               <li className="flex items-start">
                 <MapPin className="mr-2 h-5 w-5 text-church-gold shrink-0 mt-0.5" />
-                <span>30 Anderson Street Goodwood 74604</span>
+                <span>30 Anderson Street,Goodwood,7460</span>
               </li>
               <li className="flex items-center">
                 <Phone className="mr-2 h-5 w-5 text-church-gold shrink-0" />
-                <span>(27+)  21 591 2229.</span>
+                <span>(27+)  21 591 2229</span>
               </li>
               <li className="flex items-center">
                 <Mail className="mr-2 h-5 w-5 text-church-gold shrink-0" />
-                <span>stjosephgoodwood@gmail.com</span>
+                <span>info@stjosephcatholicchruch.co.za</span>
               </li>
             </ul>
             
